b683e3c445dce43491881c3aa416a167ece9f071
Luke Millar <luketmillar@gmail.com>
2020-08-29 01:03:34 -0700

    touch

--- src/Canvas/KeyboardCommands/fill.ts
+++ src/Canvas/KeyboardCommands/fill.ts
@@ -2,6 +2,7 @@ import BaseCommand from './BaseCommand'
 import toolStack from '../Tools/ToolStack'
 import FillTool from '../Tools/FillTool'
 
+
 class Fill extends BaseCommand {
     public matches(e: KeyboardEvent) {
         return !e.metaKey && !e.shiftKey && e.key === 'f'
